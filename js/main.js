d2744c7513f347d66ecb180082cae76d02ff0a03
Arafat154 <aksaraarafat0909@gmail.com>
2021-01-04 18:34:25 +0600

    new

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
    var mixer=mixitup('.content')
 });
+
+
+
+
+$('.owl-carousel').owlCarousel({
+   loop: true,
+   margin: 10,
+   nav: false,
+   dots: false,
+   autoplay: true,
+   autoplayTimeout: 2000,
+   responsive: {
+      0: {
+         items: 1
+      },
+      600: {
+         items: 3
+      },
+      1000: {
+         items: 5
+      }
+   }
+})
